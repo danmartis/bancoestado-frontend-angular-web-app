662e2400375a23c1914baa1119325a6efb6498e4
boyarzun <boyarzun@bancoestado.cl>
2020-03-03 10:37:42 -0300

    se agrega conexion gestorcontenido

--- src/app/enterprises/public/components/landing/banner-feature/banner-feature.component.ts
+++ src/app/enterprises/public/components/landing/banner-feature/banner-feature.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GestorContenidoService } from '../../../../../shared/services/gestor-contenido.service';
 
 @Component({
   selector: 'app-banner-feature',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerFeatureComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gestorContenido: GestorContenidoService) { }
 
   ngOnInit() {
+    const resultado = this.contenido();
+    console.log('resultado',resultado);
   }
 
+  async contenido(){
+    await this.gestorContenido.gestorContenido().subscribe( res => {
+      console.log('res.getDetalle()', res);
+      return res.getDetalle();
+    }),
+    err => {
+      console.log('err', err);
+      return err;
+    }
+
+  }
 }
