9aa05d9f69485828bd1d252517f31ba01aa66a53
jramirez <jose.ramirez@siigroup.cl>
2020-03-03 18:50:49 -0300

    se modifica la seccion benefits del landing

--- src/app/enterprises/public/components/landing/benefits/benefits.component.ts
+++ src/app/enterprises/public/components/landing/benefits/benefits.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenefitsComponent implements OnInit {
 
+
+  benefits = [
+    {
+      title: 'Gestión fácil',
+      description: 'de tus convenios, facturaciones, comprobantes y nóminas de recaudación.',
+      iconSrc: '/assets/imgs/svg/icono_gestion.svg',
+      lineColor: '#FFB842'
+    },
+    {
+      title: 'Trazabilidad y visibilidad',
+      description: 'de todos los procesos, hasta el abono de las recaudaciones en la cuenta que indiques.',
+      iconSrc: '/assets/imgs/svg/icono_trazabilidad.svg',
+      lineColor: '#1E63F8'
+    },
+    {
+      title: 'Seguridad y respaldo',
+      description: 'un ejecutivo siempre estará dispuesto a resolver tus dudas.',
+      iconSrc: '/assets/imgs/svg/icono_seguridad.svg',
+      lineColor: '#00D692'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
