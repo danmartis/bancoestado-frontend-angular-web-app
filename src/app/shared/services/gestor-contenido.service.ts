2c5683b1c21e2d72e64305a08e3bf84ef72726e1
jgamboa3 <jgamboa3@externos.bancoestado.cl>
2020-03-09 16:44:27 -0300

    Create endPoint Front

--- src/app/shared/services/gestor-contenido.service.ts
+++ src/app/shared/services/gestor-contenido.service.ts
@@ -9,12 +9,30 @@ import { DataResponse } from './dataResponse';
 
   export class GestorContenidoService extends BffClientService {
   
-    public urlPath = 'http://se-empresas-bff-desa.us-east-1.elasticbeanstalk.com/bff/v1/se-bff-empresas/gestorContenido';
-    //public urlPath = 'http://localhost:3000/bff/se-bff-empresas/v1/gestorContenido';
+    public urlPath = 'http://se-empresas-bff-desa.us-east-1.elasticbeanstalk.com/bff/se-bff-empresas/v1/gestor-contenido';
+    //public urlPath = 'http://localhost:3000/bff/se-bff-empresas/v1/gestor-contenido/';
   
     public gestorContenido(): Observable<DataResponse> {
       const urlApi = this.urlPath;
   
+      return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
+        .map(this.okData)
+        .catch(this.errorData)
+        .finally(() => {});
+    }
+
+    public getLanding(): Observable<DataResponse> {
+      const urlApi = this.urlPath+"landing";
+  
+      return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
+        .map(this.okData)
+        .catch(this.errorData)
+        .finally(() => {});
+    }
+
+    public getHelp(): Observable<DataResponse> {
+      const urlApi = this.urlPath+"help";
+    
       return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
         .map(this.okData)
         .catch(this.errorData)
