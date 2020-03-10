d9b81b381e9ab913985b0d32a49123c65642de45
jramirez <jose.ramirez@siigroup.cl>
2020-03-10 09:59:07 -0300

    itera sobre los items de preguntas

---
+++ src/app/enterprises/public/components/help/faq/faq.component.ts
@@ -0,0 +1,162 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxTinySliderSettingsInterface } from 'ngx-tiny-slider';
+import { ModalService } from 'src/app/shared/services/modal.service';
+
+@Component({
+  selector: 'app-faq',
+  templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.scss']
+})
+export class FaqComponent implements OnInit {
+
+  protected question : string = '';
+  // https://www.npmjs.com/package/ngx-tiny-slider
+  tinySliderConfig: NgxTinySliderSettingsInterface;
+  constructor(protected modalService:ModalService) { }
+
+  faqItems : any = [
+    {
+      icon: 'insert_drive_file',
+      category: 'Recaudaciones',
+      items: [
+        { 
+          question: 'Recauda con nosotros',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '20'
+        },
+        { 
+          question: 'Tipos de conexión disponible',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '19'
+        },
+        { 
+          question: 'Active tu convenio',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '18'
+        },
+        { 
+          question: 'Alternativas de recaudación',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '17'
+        },
+        { 
+          question: 'Beneficio para tus clientes',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '16'
+        },
+      ]
+    },
+    {
+      icon: 'credit_card',
+      category: 'Pago de cuentas',
+      items: [
+        { 
+          question: 'Inscribe tus cuentas',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '20'
+        },
+        { 
+          question: 'Paga tus cuentas',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '19'
+        },
+        { 
+          question: 'Agrupa tus cuentas',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '18'
+        },
+        { 
+          question: 'Paga tus cuentas masivamente',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '17'
+        },
+        { 
+          question: 'Enrola tu medio de pago',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '16'
+        },
+      ]
+    },
+    {
+      icon: 'wifi',
+      category: 'Servicios en línea',
+      items: [
+        { 
+          question: 'Reportes',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '20'
+        },
+        { 
+          question: 'Rendiciones',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '19'
+        },
+        { 
+          question: 'Configuración de comprobante',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '18'
+        },
+        { 
+          question: 'Configuración de convenio',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '17'
+        },
+        { 
+          question: 'Administración de perfiles',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '16'
+        },
+      ]
+    },
+    {
+      icon: 'check_circle',
+      category: 'Otros',
+      items: [
+        { 
+          question: 'Quiénes pueden operar',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '20'
+        },
+        { 
+          question: 'Nosotros',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '19'
+        },
+        { 
+          question: '¿Cómo me registro?',
+          answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus ex officia dolorem molestias soluta tempora neque commodi ipsum harum deleniti incidunt itaque, atque ab asperiores numquam perferendis id quas?',
+          views: '18'
+        }
+      ]
+    },
+  ];
+ 
+  ngOnInit() {
+    this.tinySliderConfig = {
+      arrowKeys: true,
+      autoWidth: true,
+      gutter: 15,
+      controls: false,
+      items: 4,
+      mouseDrag: true,
+      slideBy: 'page',
+      responsive: {
+        "350": {
+          "items": 3,
+          "controls": false
+        },
+        "500": {
+          "items": 3
+        }
+      },
+    };
+  }
+
+  changeQuestion(question) {
+    this.question = question;
+  }
+  closeListOpenSingle(closeList: Array<string>, open: string) {
+    this.modalService.closeListOpenSingle(closeList, open);
+  }
+
+}
