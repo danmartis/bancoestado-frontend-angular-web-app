e9cb87bc96ee43f6d96dd165636cd62523d190fc
jgamboa3 <jgamboa3@externos.bancoestado.cl>
2020-03-10 11:39:39 -0300

    add service listado de preguntas y videos

--- src/app/shared/services/gestor-contenido.service.ts
+++ src/app/shared/services/gestor-contenido.service.ts
@@ -2,25 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable'
 import { BffClientService } from './bffClient.service';
 import { DataResponse } from './dataResponse';
-
 @Injectable({
     providedIn: 'root'
   })
-
   export class GestorContenidoService extends BffClientService {
   
     public urlPath = 'http://se-empresas-bff-desa.us-east-1.elasticbeanstalk.com/bff/se-bff-empresas/v1/gestor-contenido/';
     //public urlPath = 'http://localhost:3000/bff/se-bff-empresas/v1/gestor-contenido/';
   
-    public gestorContenido(): Observable<DataResponse> {
-      const urlApi = this.urlPath;
-  
-      return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
-        .map(this.okData)
-        .catch(this.errorData)
-        .finally(() => {});
-    }
-
     public getLanding(): Observable<DataResponse> {
       const urlApi = this.urlPath+"landing";
   
@@ -29,8 +18,7 @@ import { DataResponse } from './dataResponse';
         .catch(this.errorData)
         .finally(() => {});
     }
-
-    public getHelp(): Observable<DataResponse> {
+    public getQuestions(): Observable<DataResponse> {
       const urlApi = this.urlPath+"questions";
     
       return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
@@ -38,5 +26,20 @@ import { DataResponse } from './dataResponse';
         .catch(this.errorData)
         .finally(() => {});
     }
-  
+    public getQuestionsDetails(): Observable<DataResponse> {
+      const urlApi = this.urlPath+"question-details";
+    
+      return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
+        .map(this.okData)
+        .catch(this.errorData)
+        .finally(() => {});
+    }
+    public getVideos(): Observable<DataResponse> {
+      const urlApi = this.urlPath+"videos";
+    
+      return this.http.get(urlApi, { headers: this.getHeaders(), observe: 'response' })
+        .map(this.okData)
+        .catch(this.errorData)
+        .finally(() => {});
+    }
   }
