4e8366caea642ca8b0973b1c0f41e244933788b1
boyarzun <boyarzun@bancoestado.cl>
2020-03-18 11:22:22 -0300

    pruebas unitarias

--- src/app/enterprises/public/components/help/help-details/help-details.component.spec.ts
+++ src/app/enterprises/public/components/help/help-details/help-details.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelpDetailsComponent } from './help-details.component';
+import { PageComponent } from 'src/app/shared/components/components-atom/page/page.component';
+import { HelpBannerComponent } from '../help-banner/help-banner.component';
+import { Button2Component } from 'src/app/shared/components/button/button2/button2.component';
+import { ParagraphComponent } from 'src/app/shared/components/components-atom/paragraph/paragraph.component';
+import { PageTitleComponent } from 'src/app/shared/components/components-atom/page-title/page-title.component';
 
 describe('HelpDetailsComponent', () => {
   let component: HelpDetailsComponent;
@@ -8,7 +13,14 @@ describe('HelpDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpDetailsComponent ]
+      declarations: [
+         HelpDetailsComponent,
+         PageComponent,
+         Button2Component,
+         HelpBannerComponent,
+         ParagraphComponent,
+         PageTitleComponent
+        ]
     })
     .compileComponents();
   }));
