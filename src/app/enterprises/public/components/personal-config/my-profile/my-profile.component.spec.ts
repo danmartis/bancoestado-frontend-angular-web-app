39123d9d42edc10edd561172b8424fafc4ea6e68
frank.mujica <frank.mujica@siigroup.cl>
2020-04-03 18:20:05 -0300

    Add my-profile test

--- src/app/enterprises/public/components/personal-config/my-profile/my-profile.component.spec.ts
+++ src/app/enterprises/public/components/personal-config/my-profile/my-profile.component.spec.ts
@@ -1,19 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MyProfileComponent } from './my-profile.component';
-import { ComponentsModule } from 'src/app/shared/components/components.module';
-import { InfoItemComponent } from '../info-item/info-item.component';
+import { MyProfileComponent } from "./my-profile.component";
+import { ComponentsModule } from "src/app/shared/components/components.module";
+import { InfoItemComponent } from "../info-item/info-item.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-describe('MyProfileComponent', () => {
+//services
+import { FilesService } from '../../../../../services/files/files.service';;
+
+describe("MyProfileComponent", () => {
   let component: MyProfileComponent;
   let fixture: ComponentFixture<MyProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyProfileComponent, InfoItemComponent ],
-      imports: [ComponentsModule]
-    })
-    .compileComponents();
+      declarations: [MyProfileComponent, InfoItemComponent],
+      imports: [
+        ComponentsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ], 
+      providers: [
+          { provide: FilesService }
+        ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,7 +34,28 @@ describe('MyProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  fit("should be able to download pdf", () => {
+    expect(component.downloadFile).toBeTruthy();
+  });
+
+  fit("should get the current user data", () => {
+    expect(component.getCurrentUser).toBeTruthy();
+  });
+
+  fit("the function isAdmin should return true if user is admin", () => {
+    expect(component.isAdmin).toBeTruthy();
+  });
+
+  fit("the function isAdmin should return false if user isn't admin", () => {
+    expect(!component.isAdmin).toBeFalsy();
+  });
+
+  fit("the function isAdmin should match the string 'Administrador' to verify if user is admin", () => {
+    const admin = "Administrador";
+    expect(component.isAdmin).toMatch(admin);
   });
 });
