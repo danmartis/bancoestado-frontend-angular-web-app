f9235ed994d866d143a98963941dce2b81b07313
frank.mujica <frank.mujica@siigroup.cl>
2020-04-03 18:22:26 -0300

    Add my-profile test

--- src/app/enterprises/public/components/personal-config/my-profile/my-profile.component.spec.ts
+++ src/app/enterprises/public/components/personal-config/my-profile/my-profile.component.spec.ts
@@ -34,27 +34,27 @@ describe("MyProfileComponent", () => {
     fixture.detectChanges();
   });
 
-  fit("should create", () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  fit("should be able to download pdf", () => {
+  it("should be able to download pdf", () => {
     expect(component.downloadFile).toBeTruthy();
   });
 
-  fit("should get the current user data", () => {
+  it("should get the current user data", () => {
     expect(component.getCurrentUser).toBeTruthy();
   });
 
-  fit("the function isAdmin should return true if user is admin", () => {
+  it("the function isAdmin should return true if user is admin", () => {
     expect(component.isAdmin).toBeTruthy();
   });
 
-  fit("the function isAdmin should return false if user isn't admin", () => {
+  it("the function isAdmin should return false if user isn't admin", () => {
     expect(!component.isAdmin).toBeFalsy();
   });
 
-  fit("the function isAdmin should match the string 'Administrador' to verify if user is admin", () => {
+  it("the function isAdmin should match the string 'Administrador' to verify if user is admin", () => {
     const admin = "Administrador";
     expect(component.isAdmin).toMatch(admin);
   });
