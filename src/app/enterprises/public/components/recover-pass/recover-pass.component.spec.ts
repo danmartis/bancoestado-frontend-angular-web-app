eb871160cd19ec0d28188e1e4f808d0c05d6344d
demaro <demaromail@gmail.com>
2020-04-03 15:14:05 -0300

    test

--- src/app/enterprises/public/components/recover-pass/recover-pass.component.spec.ts
+++ src/app/enterprises/public/components/recover-pass/recover-pass.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecoverPassComponent } from './recover-pass.component';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
 
 describe('RecoverPassComponent', () => {
   let component: RecoverPassComponent;
@@ -8,7 +9,8 @@ describe('RecoverPassComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecoverPassComponent ]
+      declarations: [ RecoverPassComponent ],
+      imports: [ComponentsModule]
     })
     .compileComponents();
   }));
